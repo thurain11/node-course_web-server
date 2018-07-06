94107e4053aaca80ed3e0cc3c78b1e94b7a7d31d
Thurain Hein <thurainhein097@gmail.com>
2018-07-06 12:38:15 +0630

    add Porject hbs page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about',(req,res)=>{
     });
 });
 
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+       project:"Project",
+    });
+})
+
 app.listen(port,()=>{
    console.log(`Server is running.. ${port}`);
 });
